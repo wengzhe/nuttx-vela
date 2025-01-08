3dc75d3e77015d0af400980712ffe940b3ffeb01
wangjianyu3 <wangjianyu3@xiaomi.com>
2025-01-08 18:22:12 +0800

    drivers/sensors/gnss: Macro `UINT8_MAX` for maximum value of `uint8_t crefs`
    libs/libc/gnssutils/minmea/minmea.h:17:#include <stdint.h>
    
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- drivers/sensors/gnss_uorb.c
+++ drivers/sensors/gnss_uorb.c
@@ -175,7 +175,8 @@ static int gnss_activate(FAR struct sensor_lowerhalf_s *lower,
   int ret = OK;
 
   nxmutex_lock(&upper->lock);
-  if ((upper->crefs == 255 && enable) || (upper->crefs == 0 && !enable))
+  if ((upper->crefs == UINT8_MAX && enable) ||
+      (upper->crefs == 0 && !enable))
     {
       ret = -EINVAL;
     }
@@ -240,7 +241,7 @@ static int gnss_open(FAR struct file *filep)
     }
 
   nxmutex_lock(&upper->lock);
-  if (upper->crefs >= 255)
+  if (upper->crefs >= UINT8_MAX)
     {
       ret = -EMFILE;
       kmm_free(user);
